9bc7f18d5e7fe5456f8d8dff3f2f3265e9879f82
Jordan Deutsch <jordanbdeutsch@gmail.com>
2015-03-29 16:35:51 -0400

    made the sun

--- script.js
+++ script.js
@@ -10,11 +10,19 @@ canvas.setAttribute('id', 'canvas');
 c.fillStyle = "black";
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+//radial gradient
+var grd = c.createRadialGradient(120,120,70,120,120,100);
+grd.addColorStop(0,"rgba(255,215,0, 1)");
+grd.addColorStop(1,"rgba(255,215,0,0.4)");
+
+
 //make a circle (the sun)
 c.beginPath();
-c.arc(100, 100, 50, Math.PI*2, false);
-c.fillStyle = "yellow";
+c.arc(120, 120, 100, Math.PI*2, false);
+c.fillStyle = grd;
 c.fill();
+
+
 
 // make some stars
 
